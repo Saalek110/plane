831b5514425178e0192f2b62001c57517b1de22c
Saalek110 <160769553+Saalek110@users.noreply.github.com>
2024-02-21 22:19:49 +0330

    Update index.php

--- index.php
+++ index.php
@@ -2,17 +2,15 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="my_css.css">
+
 <style>
-canvas {
-    border:1px solid #d3d3d3;
-    background-color: #f1f1f1;
-}
+<?php
+include"my_css.php";
+?>
 </style>
 </head>
 <body onload="startGame()">
-<img id="img2" src="images/war_plane.png" alt="plane" width="40" height="40">
-<p id="gamearea">--</p>
+<img id="img2" src="images/war_plane.png" alt="plane" width="30" height="9">
  <script type='text/javascript'>
 var hero;
 var myObstacles = [];
@@ -20,7 +18,7 @@ var myScore;
 let heart=1000;
 
 function startGame() {
-    hero = new hero(100, 33, "red", 10, 235 );
+    hero = new hero(100, 33, "red", 10,120);
        //hero.elevation = ;
        //boing.gravity = 0.05;
  //  myScore = new component("20px", "Consolas", "black", 280, 40, "text");
@@ -54,17 +52,22 @@ function everyinterval(n) {
     return false;
 }
 function accelerate(n) {
-    hero.ahead = n;
- 
+    hero.gravity = n;
+ if(n==0)
+ {
+    hero.gravity = 0;
+   hero.gravitySpeed = 0;
+
+ }//n==0
 }
 </script>
 <br><br><br>
 <center>
 <p id="the_heart"></p>
 
-<button type="button" style=" font-size:35px; height:100px; width:220px" onclick="accelerate(-1)">up</button>
+<button type="button" style=" font-size:35px; height:100px; width:220px" onclick="accelerate(-0.04)">up</button>
 <button type="button" style=" font-size:12px; height:55px; width:120px" onclick="accelerate(0)">stop</button>
-<button type="button" style=" font-size:35px; height:100px; width:220px" onclick="accelerate(1)">down</button>
+<button type="button" style=" font-size:35px; height:100px; width:220px" onclick="accelerate(0.04)">down</button>
 </center>
 <p>Use the up and down button to stay in the air</p>
 <p>How long can you stay alive?</p>
